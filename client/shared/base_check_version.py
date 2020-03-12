e7ce74d98b7c4a176d1baa06da5df7c4b9ca4d54
Wei Gao <wei.gao@windriver.com>
2020-03-12 19:52:45 +0000

    Drop those version check codes, which makes issue while run it by python3.
    Signed-off-by: Wei Gao <wei.gao@windriver.com>

--- client/shared/base_check_version.py
+++ client/shared/base_check_version.py
@@ -14,22 +14,6 @@ class base_check_python_version:
         except AttributeError:
             pass  # pre 2.0, no neat way to get the exact number
 
-        # The change to prefer 2.4 really messes up any systems which have both
-        # the new and old version of Python, but where the newer is default.
-        # This is because packages, libraries, etc are all installed into the
-        # new one by default. Some things (like running under mod_python) just
-        # plain don't handle python restarting properly. I know that I do some
-        # development under ipython and whenever I run (or do anything that
-        # runs) 'import common  # pylin: disable=W0611' it restarts my shell. Overall, the change was
-        # fairly annoying for me (and I can't get around having 2.4 and 2.5
-        # installed with 2.5 being default).
-        if not version or version < (2, 4) or version >= (3, 0):
-            try:
-                # We can't restart when running under mod_python.
-                from mod_python import apache
-            except ImportError:
-                self.restart()
-
     def extract_version(self, path):
         match = re.search(r'/python(\d+)\.(\d+)$', path)
         if match:
